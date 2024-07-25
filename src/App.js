f2e0d9a643cb68ccd4e6860a1aef14ffc3b03ba6
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2024-07-25 13:04:52 -0400

    add create and close room

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import './App.css';
 import io from 'socket.io-client';
 import CardList from './components/CardList';
@@ -6,7 +7,9 @@ import CountdownTimer from './components/CountdownTimer';
 
 const socket = io(process.env.REACT_APP_API_URL);
 
-const App = ({ votingInstanceName }) => {
+const App = () => {
+  const { votingInstanceName } = useParams();
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [participants, setParticipants] = useState([]);
   const [joinedParticipants, setJoinedParticipants] = useState([]);
@@ -15,8 +18,12 @@ const App = ({ votingInstanceName }) => {
   const [showResults, setShowResults] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
   const [error, setError] = useState('');
+  const [roomOpen, setRoomOpen] = useState(false);
 
   useEffect(() => {
+    fetchRoomDetails();
+    updateResults();
+
     socket.on('timerTick', () => {
       setTimerStarted(true);
     });
@@ -76,8 +83,24 @@ const App = ({ votingInstanceName }) => {
     updateResults();
   };
 
+  const fetchRoomDetails = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/rooms/${votingInstanceName}`
+      );
+      if (!response.ok) throw new Error('Failed to fetch room details');
+      const data = await response.json();
+      setRoomOpen(data.room.open);
+      setParticipants(data.participants);
+      setJoinedParticipants(data.participants);
+    } catch (err) {
+      setError('Error fetching room details: ' + err.message);
+    }
+  };
+
   const updateResults = useCallback(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/results/${votingInstanceName}`)
+    console.log('Fetching results');
+    fetch(`${process.env.REACT_APP_API_URL}/rooms/${votingInstanceName}`)
       .then((res) => res.json())
       .then((data) => {
         setParticipants(data);
@@ -108,6 +131,23 @@ const App = ({ votingInstanceName }) => {
       });
   };
 
+  const closeRoom = () => {
+    fetch(
+      `${process.env.REACT_APP_API_URL}/rooms/${votingInstanceName}/close`,
+      {
+        method: 'PATCH',
+      }
+    )
+      .then((res) => {
+        if (!res.ok) throw new Error('Failed to close room');
+        alert('Hasta pronto');
+        navigate('/');
+      })
+      .catch((error) => {
+        setError('Error closing room: ' + error.message);
+      });
+  };
+
   const calculateAverage = useCallback((votes) => {
     const validVotes = votes.filter((vote) => vote !== null);
     if (validVotes.length === 0) return 0;
@@ -130,90 +170,99 @@ const App = ({ votingInstanceName }) => {
       <h1>Planning Poker</h1>
       <h2>Voting Instance Name: {votingInstanceName}</h2>
       {error && <p className="error">{error}</p>}
-      {joinedParticipants.length > 0 && (
-        <div>
-          <h3>Joined Participants:</h3>
-          <div className="participant-labels">
-            {joinedParticipants.map((participant, index) => (
-              <span
-                key={index}
-                className={`label ${participant.hasVoted ? 'voted' : ''}`}
-              >
-                {participant.name}
-              </span>
-            ))}
-          </div>
-        </div>
-      )}
-      {!joined ? (
-        <div>
-          <input
-            type="text"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button onClick={handleJoin}>Join</button>
-        </div>
+      {!roomOpen ? (
+        <p>This room is closed.</p>
       ) : (
-        <div>
-          <CardList
-            values={[1, 2, 3, 5, 8, 13, 21]}
-            onSelect={handleSelect}
-            selectedValue={selectedValue}
-          />
-          <button onClick={startTimer} disabled={timerStarted}>
-            Start Timer
-          </button>
-          <button onClick={deleteVotes}>Delete Votes</button>
-          {timerStarted && <CountdownTimer onTimerEnd={handleTimerEnd} />}
-          {showResults && (
-            <table className="text-left margin-auto">
-              <thead>
-                <tr>
-                  <th>Usuario</th>
-                  <th>Opción</th>
-                </tr>
-              </thead>
-              <tbody>
-                {participants.map((participant) => (
-                  <tr key={participant.name}>
-                    <td>{participant.name}</td>
-                    <td className="text-right">
-                      {participant.vote !== null
-                        ? participant.vote
-                        : 'No selection'}
-                    </td>
-                  </tr>
+        <>
+          {joinedParticipants.length > 0 && (
+            <div>
+              <h3>Joined Participants:</h3>
+              <div className="participant-labels">
+                {joinedParticipants.map((participant, index) => (
+                  <span
+                    key={index}
+                    className={`label ${participant.hasVoted ? 'voted' : ''}`}
+                  >
+                    {participant.name}
+                  </span>
                 ))}
-                <tr>
-                  <td>
-                    <b>Promedio Real</b>
-                  </td>
-                  <td className="text-right">
-                    <b>
-                      {calculateAverage(
-                        participants.map((p) => p.vote)
-                      ).toFixed(2)}
-                    </b>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>Promedio Planning Poker</b>
-                  </td>
-                  <td className="text-right">
-                    <b>
-                      {calculatePlanningPokerAverage(
-                        participants.map((p) => p.vote)
-                      ).toFixed(2)}
-                    </b>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+              </div>
+            </div>
           )}
-        </div>
+          {!joined ? (
+            <div>
+              <input
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <button onClick={handleJoin}>Join</button>
+            </div>
+          ) : (
+            <div>
+              <CardList
+                values={[1, 2, 3, 5, 8, 13, 21]}
+                onSelect={handleSelect}
+                selectedValue={selectedValue}
+              />
+              <button onClick={startTimer} disabled={timerStarted}>
+                Start Timer
+              </button>
+              &nbsp; &nbsp;
+              <button onClick={deleteVotes}>Reset All Votes</button>
+              &nbsp; &nbsp;
+              <button onClick={closeRoom}>Close Room</button>
+              {timerStarted && <CountdownTimer onTimerEnd={handleTimerEnd} />}
+              {showResults && (
+                <table className="text-left margin-auto">
+                  <thead>
+                    <tr>
+                      <th>Usuario</th>
+                      <th>Opción</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {participants.map((participant) => (
+                      <tr key={participant.name}>
+                        <td>{participant.name}</td>
+                        <td className="text-right">
+                          {participant.vote !== null
+                            ? participant.vote
+                            : 'No selection'}
+                        </td>
+                      </tr>
+                    ))}
+                    <tr>
+                      <td>
+                        <b>Promedio Real</b>
+                      </td>
+                      <td className="text-right">
+                        <b>
+                          {calculateAverage(
+                            participants.map((p) => p.vote)
+                          ).toFixed(2)}
+                        </b>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Promedio Planning Poker</b>
+                      </td>
+                      <td className="text-right">
+                        <b>
+                          {calculatePlanningPokerAverage(
+                            participants.map((p) => p.vote)
+                          ).toFixed(2)}
+                        </b>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              )}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
 
 const Home = () => {
+  const [instanceName, setInstanceName] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleCreateRoom = async () => {
+    if (!instanceName) {
+      setError('Please enter a name for the voting instance.');
+      return;
+    }
+
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/rooms`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          slug: instanceName,
+          open: true,
+          create_at: new Date(),
+        }),
+      });
+
+      if (response.ok) {
+        navigate(`/${instanceName}`);
+      } else {
+        const errorData = await response.json();
+        setError(`Error: ${errorData.message}`);
+      }
+    } catch (err) {
+      setError('Error creating the room. Please try again.');
+    }
+  };
+
   return (
     <div className="App">
       <h1>Welcome to Planning Poker</h1>
-      <p>Please enter a voting instance name in the URL to start voting.</p>
+      <p>Enter the name for a voting instance to create one:</p>
+      {error && <p className="error">{error}</p>}
+      <input
+        type="text"
+        value={instanceName}
+        onChange={(e) => setInstanceName(e.target.value)}
+        placeholder="Enter instance name"
+      />
+      <button onClick={handleCreateRoom}>Create Room</button>
     </div>
   );
 };
